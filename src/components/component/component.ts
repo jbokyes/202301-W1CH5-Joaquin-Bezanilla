7908b4bdb90f64cbc07fda663353eba8d1fa1169
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-04 14:07:56 +0100

    Added component.ts in component folder

---
+++ src/components/component/component.ts
@@ -0,0 +1,19 @@
+export abstract class Component {
+  selector!: string;
+  element!: HTMLElement;
+  template!: string;
+
+  render(place: globalThis.InsertPosition) {
+    const setElement = {
+      afterbegin: () =>
+        document.querySelector(this.selector)?.firstElementChild,
+      afterend: () => document.querySelector(this.selector)?.nextElementSibling,
+      beforebegin: () =>
+        document.querySelector(this.selector)?.nextElementSibling,
+      beforeend: () => document.querySelector(this.selector)?.lastElementChild,
+    };
+    const initialElement = document.querySelector(this.selector) as HTMLElement;
+    initialElement.insertAdjacentHTML(place, this.template);
+    this.element = setElement[place]() as HTMLElement;
+  }
+}
